c918195c795c67a1eeabc068f39bc4399d381270
민준홍 <120389042+dpxkals@users.noreply.github.com>
2025-10-15 18:15:27 +0900

    shadowMan class 리펙터링 및 파일 이름 변경

--- main.py
+++ main.py
@@ -1,49 +1,13 @@
 from pico2d import *
-import random
+
+from shadow_man import ShadowMan
+
 
 # Game object class here
 # 1. 객체의 도출 - 추상화
 # 2. 속성을 도출 - 추상화
 # 3. 행위를 도출
 # 4. 클래스를 제작
-
-class ShadowMan:
-    def __init__(self):
-        self.x, self.y = 200, 200
-        self.is_walking = False
-        self.idle_image = load_image('그림자검객_idlePNG.png')
-        self.walk_image = load_image('그림자검객_walk.png')
-        self.idle_sprite_size = (340, 360)
-        self.walk_sprite_size = (227, 260)
-        self.frame_idle = 3
-        self.frame_walk = 5
-        self.current_image = self.idle_image
-        self.current_sprite_size = self.idle_sprite_size
-        self.frame = self.frame_idle
-        self.current_frame = 0
-
-    def set_walking(self, walking):
-        self.is_walking = walking
-        if walking:
-            self.current_image = self.walk_image
-            self.current_sprite_size = self.walk_sprite_size
-            self.frame = self.frame_walk
-        else:
-            self.current_image = self.idle_image
-            self.current_sprite_size = self.idle_sprite_size
-            self.frame = self.frame_idle
-
-    def update(self):
-        self.current_frame = (self.current_frame + 1) % self.frame
-        if self.is_walking:
-            self.x += 10
-
-    def draw(self):
-        sprite_w, sprite_h = self.current_sprite_size
-        self.current_image.clip_draw(
-            self.current_frame * sprite_w, 0, sprite_w, sprite_h,
-            self.x, self.y, 400, 400
-        )
 
 def handle_events():
     global running
---
+++ shadow_man.py
@@ -0,0 +1,40 @@
+from pico2d import load_image
+
+
+class ShadowMan:
+    def __init__(self):
+        self.x, self.y = 200, 200
+        self.is_walking = False
+        self.idle_image = load_image('그림자검객_idlePNG.png')
+        self.walk_image = load_image('그림자검객_walk.png')
+        self.idle_sprite_size = (340, 360)
+        self.walk_sprite_size = (227, 260)
+        self.frame_idle = 3
+        self.frame_walk = 5
+        self.current_image = self.idle_image
+        self.current_sprite_size = self.idle_sprite_size
+        self.frame = self.frame_idle
+        self.current_frame = 0
+
+    def set_walking(self, walking):
+        self.is_walking = walking
+        if walking:
+            self.current_image = self.walk_image
+            self.current_sprite_size = self.walk_sprite_size
+            self.frame = self.frame_walk
+        else:
+            self.current_image = self.idle_image
+            self.current_sprite_size = self.idle_sprite_size
+            self.frame = self.frame_idle
+
+    def update(self):
+        self.current_frame = (self.current_frame + 1) % self.frame
+        if self.is_walking:
+            self.x += 10
+
+    def draw(self):
+        sprite_w, sprite_h = self.current_sprite_size
+        self.current_image.clip_draw(
+            self.current_frame * sprite_w, 0, sprite_w, sprite_h,
+            self.x, self.y, 400, 400
+        )
