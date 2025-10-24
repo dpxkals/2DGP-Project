487c0b466763c0b0056790a6fe49551b5f13be4e
민준홍 <120389042+dpxkals@users.noreply.github.com>
2025-10-25 02:21:11 +0900

    장검캐릭터 dash 이벤트,class, 변수/ 선언 및 정의 완료

--- shadow_man.py
+++ shadow_man.py
@@ -1,5 +1,5 @@
 from pico2d import load_image
-from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT, SDLK_a, SDLK_d
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT, SDLK_a, SDLK_d, SDLK_LCTRL
 
 from state_machine import StateMachine
 
@@ -12,6 +12,55 @@ def a_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_a
 def d_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_d
+def lctrl_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LCTRL
+def lctrl_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LCTRL
+def dash_end(e):
+    return e[0] == 'DASH_END'
+
+
+class Dash:
+    def __init__(self, shadowMan):
+        self.shadowMan = shadowMan
+        self.dash_speed = 100  # 대시 속도
+        self.dash_duration = 3  # 대시 지속 프레임
+        self.dash_timer = 0
+
+    def enter(self, e):
+        # 대시 이미지가 있다면 변경 (없다면 walk 이미지 사용)
+        self.shadowMan.current_image = self.shadowMan.dash_image
+        self.shadowMan.current_sprite_size = self.shadowMan.dash_sprite_size
+        self.shadowMan.frame = self.shadowMan.frame_dash
+        self.dash_timer = self.dash_duration
+        # 현재 바라보는 방향으로 대시
+
+    def exit(self, e):
+        pass
+
+    def do(self):
+        self.shadowMan.current_frame = (self.shadowMan.current_frame + 1) % self.shadowMan.frame
+        # 대시 이동
+        self.shadowMan.x += self.shadowMan.face_dir * self.dash_speed
+        self.dash_timer -= 1
+
+        # 대시 시간이 끝나면 IDLE로 전환
+        if self.dash_timer <= 0:
+            self.shadowMan.state_machine.handle_state_event(('DASH_END', None))
+
+    def draw(self):
+        sprite_w, sprite_h = self.shadowMan.current_sprite_size
+        if self.shadowMan.face_dir == 1:
+            self.shadowMan.current_image.clip_draw(
+                self.shadowMan.current_frame * sprite_w, 0, sprite_w, sprite_h,
+                self.shadowMan.x, self.shadowMan.y, 300, 300
+            )
+        else:
+            self.shadowMan.current_image.clip_composite_draw(
+                self.shadowMan.current_frame * sprite_w, 0, sprite_w, sprite_h,
+                0, 'h',
+                self.shadowMan.x, self.shadowMan.y, 300, 300
+            )
 
 class Walk:
 
@@ -86,10 +135,13 @@ class ShadowMan:
         self.x, self.y = 200, 300
         self.idle_image = load_image('그림자검객_idle.png')
         self.walk_image = load_image('그림자검객_walk.png')
+        self.dash_image = load_image('그림자검객_dash.png')
         self.idle_sprite_size = (340, 360)
         self.walk_sprite_size = (227, 260)
+        self.dash_sprite_size = (400, 285)
         self.frame_idle = 3
         self.frame_walk = 5
+        self.frame_dash = 2
         # 이동 방향 변수
         self.dir = 0
         # 바라보는 방향 변수
@@ -103,11 +155,13 @@ class ShadowMan:
         # 상태 변화 테이블
         self.IDLE = Idle(self)
         self.WALK = Walk(self)
+        self.DASH = Dash(self)
         self.state_machine = StateMachine(
             self.IDLE,
             {
-                self.IDLE:{a_down : self.WALK, d_down : self.WALK},
-                self.WALK:{a_up : self.IDLE, d_up : self.IDLE}
+                self.IDLE: {a_down: self.WALK, d_down: self.WALK, lctrl_down: self.DASH},
+                self.WALK: {a_up: self.IDLE, d_up: self.IDLE, lctrl_down: self.DASH},
+                self.DASH: {dash_end: self.IDLE}
             }
         )  # 상태머신 생성 및 초기 시작 상태 설정
 
