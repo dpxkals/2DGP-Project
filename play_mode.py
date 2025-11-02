337f7860c731425b50b68fdbd77b56bf35fb24d1
민준홍 <120389042+dpxkals@users.noreply.github.com>
2025-11-02 17:15:37 +0900

    메인을 분리 및 playmode 게임월다와 프레임워크 적용

---
+++ play_mode.py
@@ -0,0 +1,54 @@
+from pico2d import *
+
+from back_ground import background
+from shadow_man import ShadowMan
+from short_sword import short_sword
+
+import game_world
+
+import game_framework
+
+# Game object class here
+# 1. 객체의 도출 - 추상화
+# 2. 속성을 도출 - 추상화
+# 3. 행위를 도출
+# 4. 클래스를 제작
+
+def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        else:
+            shadow_man.handle_event(event)
+
+def init():
+    global shadow_man
+
+    # 객체들을 생성
+    bg = background()
+    game_world.add_object(bg, 0)
+
+    shadow_man = ShadowMan()
+    game_world.add_object(shadow_man, 1)
+
+    sword = short_sword()
+    game_world.add_object(sword, 1)
+
+def finish():
+    game_world.clear()
+
+def update():
+    game_world.update()
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+def pause(): pass
+def resume(): pass
