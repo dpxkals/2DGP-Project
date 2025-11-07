b6f2611d5fd91f5ac4b029aa8b4b74d5b5ba7266
민준홍 <120389042+dpxkals@users.noreply.github.com>
2025-11-07 21:12:57 +0900

    collision 추가

--- game_world.py
+++ game_world.py
@@ -6,10 +6,18 @@ def add_object(o, depth):
 def add_objects(ol, depth):
     world[depth] += ol
 
+def remove_collision_object(o):
+    for pairs in collision_pairs.values():
+        if o in pairs[0]:
+            pairs[0].remove(o)
+        if o in pairs[1]:
+            pairs[1].remove(o)
+
 def remove_object(o):
     for layer in world:
         if o in layer:
             layer.remove(o)
+            remove_collision_object(o)
             return
 
     raise Exception("World 에 존재하지 않는 오브젝트를 지우려고 시도함")
@@ -27,5 +35,38 @@ def render():
 
 
 def clear():
+    global world
+
     for layer in world:
         layer.clear()
+
+def collide(a, b):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b : return False
+    if right_a < left_b : return False
+    if top_a < bottom_b : return False
+    if bottom_a > top_b : return False
+
+    return True # 충돌 발생
+
+collision_pairs = {}
+
+def add_collision_pairs(group, a, b):
+    if group not in collision_pairs: # 처음 등록되는 그룹
+        collision_pairs[group] = [[], []] # 리스트 쌍 생성
+    if a:
+        collision_pairs[group][0].append(a)
+    if b:
+        collision_pairs[group][1].append(b)
+
+
+def handle_collisions():
+    # 등록된 모든 충돌 그룹에 대해 충돌 검사 실행
+    for group, pairs in collision_pairs.items():
+        for a in pairs[0]:
+            for b in pairs[1]:
+                if collide(a, b):
+                    a.handle_collision(group, b)
+                    b.handle_collision(group, a)
