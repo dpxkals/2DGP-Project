198676c21f51a29cde6d4a51a881c408bebd95e6
민준홍 <120389042+dpxkals@users.noreply.github.com>
2025-12-03 18:59:46 +0900

    모드 선택/ 버튼 사운드 추가

--- title_mode.py
+++ title_mode.py
@@ -6,6 +6,8 @@ import game_data  # 선택한 모드를 저장하기 위해 임포트
 image = None
 font = None
 bgm = None
+button_sound = None
+select_mode_sound = None
 
 menu_selection = 0  # 0: 1 vs 1,  1: AI Mode
 
@@ -50,6 +52,8 @@ def draw():
 
 def handle_events():
     global menu_selection
+    global button_sound
+    global select_mode_sound
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -61,15 +65,27 @@ def handle_events():
             # 메뉴 이동 (위/아래)
             elif event.key == SDLK_UP:
                 menu_selection = 0
+                button_sound = load_wav('Sound/button.wav')
+                button_sound.set_volume(50)
+                button_sound.play()
             elif event.key == SDLK_DOWN:
                 menu_selection = 1
+                button_sound = load_wav('Sound/button.wav')
+                button_sound.set_volume(50)
+                button_sound.play()
 
             # 선택 확정 (스페이스바)
             elif event.key == SDLK_SPACE:
                 if menu_selection == 0:
                     game_data.game_mode = 'PVP'
+                    select_mode_sound = load_wav('Sound/selectmode.wav')
+                    select_mode_sound.set_volume(50)
+                    select_mode_sound.play()
                 else:
                     game_data.game_mode = 'AI'
+                    select_mode_sound = load_wav('Sound/selectmode.wav')
+                    select_mode_sound.set_volume(50)
+                    select_mode_sound.play()
 
                 # 캐릭터 선택 모드로 이동!
                 game_framework.change_mode(select_mode)
